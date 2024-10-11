2f1372489eaa7296065a888411a58283e6dcbf9f
asdfish <lgray3420@gmail.com>
2024-10-11 06:06:01 -0400

    added string replace tests

--- test.c
+++ test.c
@@ -1,13 +1,13 @@
 #include <orchestra.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
   o_string str;
   o_string_init(&str);
-  o_string_set(&str, "hello");
-  o_string_cat(&str, " world");
-  o_string_delete(&str, 0, 1);
-  o_string_insert(&str, 0, "s");
+  o_string_set(&str, "hello world fdas worldly");
+  o_string_replace_all(&str, "world", "worlds");
+  o_string_replace_all(&str, "w", "wall");
   printf("%s\n",str.contents);
   o_string_uninit(&str);
   return 0;
